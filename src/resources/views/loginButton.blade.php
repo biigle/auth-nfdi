b40761a8dc0f408a6af09b21bf4d6036a4c2645b
Martin Zurowietz <martin@zurowietz.de>
2025-03-12 11:58:58 +0100

    Improve UI

--- src/resources/views/loginButton.blade.php
+++ src/resources/views/loginButton.blade.php
@@ -1,6 +1,8 @@
-@if ($errors->has('nfdi-id'))
-    <p class="text-danger text-center">{{ $errors->first('nfdi-id') }}</p>
-@endif
-<a style="display: block; text-align: center; margin-top: 1em;" href="{{ route('nfdi-redirect') }}" title="Log in via NFDI Login">
-    <img style="width: 216px" src="{{ cachebust_asset('vendor/auth-nfdi/IAM_9-1.png') }}">
-</a>
+<div style="text-align: center; margin-top: 1em;">
+    <a style="display: block;" href="{{ route('nfdi-redirect') }}" title="Log in via NFDI Login">
+        <img style="width: 216px" src="{{ cachebust_asset('vendor/auth-nfdi/IAM_9-1.png') }}">
+    </a>
+    @if ($errors->has('nfdi-id'))
+        <p class="text-danger text-center">{{ $errors->first('nfdi-id') }}</p>
+    @endif
+</div>
